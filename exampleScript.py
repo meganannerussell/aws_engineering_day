b7b6cbe2a493776964fca5a345664e758f50352e
Scott Eyono <scott.eyono@gmail.com>
2025-09-05 11:47:04 +0200

    Testing something

--- exampleScript.py
+++ exampleScript.py
@@ -7,7 +7,17 @@ payload = {
     "anthropic_version": "bedrock-2023-05-31",
     "max_tokens": 512,
     "messages": [
-        {"role": "user", "content": [{"type": "text", "text": "Hello, Claude!"}]}
+        {"role": "user", "content": [
+            {"type": "text", "text": "It works on my skin type"},
+            {"type": "text", "text": "I'm not sure."},
+            {"type": "text", "text": "I buy what I can to make My girlfriend or wife smell good"},
+            {"type": "text", "text": "The brand of the product itself"},
+            {"type": "text", "text": "These products stand out because they're high-quality and look prefect on my skin."},
+            {"type": "text", "text": "the smell on me"},
+            {"type": "text", "text": "it seems like it was made just for me"},
+            {"type": "text", "text": "They will be nuce"},
+            {"type": "text", "text": "From the input above, can you give me a theme for the responses?"},
+            ]}
     ],
 }
 
